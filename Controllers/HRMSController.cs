6eef50bc19c407ef6c7f29666e8c563e1624560d
khushboogoelCG <khushboo.goel@cginfinity.com>
2023-08-06 18:05:06 +0530

    code changes

---
+++ Controllers/HRMSController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SfeAdminPortal.Models;
+
+namespace SfeAdminPortal.Controllers
+{
+    public class HRMSController : Controller
+    {
+        private readonly DBContext _context;
+        public HRMSController(DBContext dBContext)
+        {
+            _context = dBContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
