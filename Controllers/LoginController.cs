e2073f37d44b3333e986a3d810e562d28da89fb4
khushboogoelCG <khushboo.goel@cginfinity.com>
2023-08-08 23:47:13 +0530

    API & Authentication code

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SfeAdminPortal.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace SfeAdminPortal.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly DBContext _context;
+        public LoginController(DBContext Mycontext)
+        {
+            _context = Mycontext;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult LoginSubmit()
+        {
+            string Name = Request.Form["email"];
+            string Pass = Request.Form["Psw"];
+            Users tbl = new Users();
+            tbl=_context.Users.Where(s => s.UserName == Name && s.Password == Pass).FirstOrDefault();
+            if(tbl==null)
+            {
+                TempData["Message"]="Invalid User Credentials!";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace SfeAdminPortal.Models
+{
+    public class Users
+    {
+        [Key]
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
